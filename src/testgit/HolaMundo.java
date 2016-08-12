c488aa7d2c3bcebb4bb561d0f69db8d36364261b
selefaccioli <selefaccioli@gmail.com>
2016-08-12 20:19:53 -0300

    posta que es la ultima

--- src/testgit/HolaMundo.java
+++ src/testgit/HolaMundo.java
@@ -1,10 +1,20 @@
 package testgit;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {		
-		boolean rta = false;
-		int i = 0;
+		Scanner sc = new Scanner(System.in);
+	
+		boolean rta = false;	
+		
+		int i = 34;
+		
+		String mark = sc.nextLine();
+		
+		rta = mark.equalsIgnoreCase("Martin");
+		
 		i = i/0;
 
 	}
